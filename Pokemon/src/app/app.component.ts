a9638ea1e1d0cede81b8b2f359f3441d3c0d2f89
LongoL4 <longo.luca@iisgalvanimi.edu.it>
2021-12-01 12:53:52 +0000

    CorrezioneVerifica

---
+++ Pokemon/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {GoogleMap} from "@angular/google-maps";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Pokemon';
+}
