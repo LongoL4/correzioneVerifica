62b8c961da4085c3b99454f76f2daeb1525d1647
LongoL4 <longo.luca@iisgalvanimi.edu.it>
2021-12-03 10:52:02 +0000

    CorrezioneVerifica

--- Pokemon/src/app/app.component.ts
+++ Pokemon/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient} from '@angular/common/http';
 import { Component } from '@angular/core';
-import {GoogleMap} from "@angular/google-maps";
+import {GoogleMap, MapMarker} from "@angular/google-maps";
+import {observable} from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,25 @@ import {GoogleMap} from "@angular/google-maps";
 })
 export class AppComponent {
   title = 'Pokemon';
+
+  center: google.maps.LatLngLiteral = { lat: 35.71407782901707,lng: 139.81065517679536}
+  zoom = 8;
+  vetbuca : google.maps.MarkerOptions[];
+  obsBuche: Observable<Buche>;
+
+  constructor(public http: HttpClient) {
+    this.vetbuca = [];
+
+  }
+
+  visbuche = (data: Buche[])=>{
+
+    console.log(data);
+    
+
+  }
+
+
+
+
 }
